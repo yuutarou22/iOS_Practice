a342c08c5007af1de4e1c3afc32d2efe1129ecc1
YutaroM <yutaroapp@gmail.com>
2020-11-06 08:24:59 +0900

    カメラ機能の追加

---
+++ iOS_Practice/iOS_Practice/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  iOS_Practice
+//
+//  Created by YutaroMatsunaga on 2020/11/05.
+//  Copyright © 2020 YutaroMatsunaga. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
